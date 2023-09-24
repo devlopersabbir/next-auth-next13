2630fb0029d0f33a54f929a0c38bf95b4226da5e
devlopersabbir <devlopersabbir@gmail.com>
2023-09-25 01:53:04 +0600

    routes options for provider

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,26 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credential",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your-username",
+        },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        return null;
+      },
     }),
   ],
 };
